1c61cb79f6048035713699f06235aa62842e436b
vitaliyt <vitaliyt@vitaliyt-pc.(none)>
2010-12-22 22:08:13 +0200

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoFolder::Application.config.secret_token = '7f41cdf13abe47fc9c25eff98c802328664f45ed995ba5105d8fb92dcedd6db637ac5e885597bf402e8f749ff4fbc6e15329c309296ecc5d0169f6865b7e1db9'
